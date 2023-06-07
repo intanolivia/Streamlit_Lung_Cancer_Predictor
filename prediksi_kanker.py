6dbfccf4d2f87aca71b0e30cd89e09223b2fa0ab
chocolivia <128565993+intanolivia@users.noreply.github.com>
2023-06-07 07:55:31 +0700

    Add files via upload

---
+++ prediksi_kanker.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+
+def calculate_lung_cancer_score(age, gender, smoke, asbestos, family):
+    """
+    This function calculates the Gupta Postoperative Respiratory Failure Risk based on the input parameters.
+    """
+    # The following is pseudo-code for calculating the score.
+    # Replace with the actual calculation algorithm.
+
+    score = 0
+    score += age
+    score += gender
+    score += smoke
+    score += asbestos
+    score += family 
+
+    return score
+
+st.title('Lung Cancer Predictor')
+st.write('The streamlit code provided is a sample implementation of a Lung Cancer Risk Calculator. It uses a logistic regression model to predict the probability of a patient having lung cancer based on their age, gender, smoking history, and exposure history.')
+
+age = st.slider('Age', 0, 100, 30)
+gender = st.selectbox('Gender', [(0, 'Male'), (1, 'Female')])
+smoke = st.selectbox('Smoke', [(0, 'No'), (1, 'Yes')])
+asbestos = st.selectbox('Exposure to asbestos', [(0, 'No'), (1, 'Yes')])
+family = st.selectbox('Family history of lung cancer', [(0, 'No'), (1, 'Yes')])
+
+if st.button('Calculate Score'):
+    score = calculate_lung_cancer_score(age, gender[0], smoke[0], asbestos[0], family [0])
+    st.write('The Lung Cancer Risk is: ', score)
